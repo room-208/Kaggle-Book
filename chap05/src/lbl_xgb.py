f7043c3c98e8157971e4a81b68755bddf6a1f2e5
room-208 <yumalegomindstorm@gmail.com>
2022-10-16 19:29:51 +0900

    v2を作成しました

--- chap05/src/lbl_xgb.py
+++ chap05/src/lbl_xgb.py
@@ -3,7 +3,7 @@ import xgboost as xgb
 from sklearn import metrics, preprocessing
 
 
-def run(fold):
+def run_v1(fold):
     df = pd.read_csv("../input/train_folds.csv")
 
     if 0:
@@ -41,6 +41,54 @@ def run(fold):
     print(auc)
 
 
+def run_v2(fold):
+    df = pd.read_csv("../input/adult_folds.csv")
+
+    if 0:
+        print(df)
+
+    num_cols = [
+        "fnlwgt",
+        "age",
+        "capitalgain",
+        "capitalloss",
+        "hoursperweek",
+    ]
+
+    features = [f for f in df.columns if f not in ["target", "kfold"]]
+
+    for col in features:
+        df[col] = df[col].astype(str).fillna("NONE")
+
+    for col in features:
+        if col not in num_cols:
+            lbl = preprocessing.LabelEncoder()
+            lbl.fit(df[col].values)
+            df[col] = lbl.transform(df[col].values)
+
+    df_train = df[df["kfold"] != fold].reset_index(drop=True)
+    df_valid = df[df["kfold"] == fold].reset_index(drop=True)
+
+    if 0:
+        print(df_train)
+        print(df_valid)
+
+    X_train = df_train[features].values
+    y_train = df_train["target"].values
+    X_valid = df_valid[features].values
+    y_valid = df_valid["target"].values
+
+    # model = xgb.XGBClassifier(n_jobs=-1, max_depth=7, n_estimators=200)
+    model = xgb.XGBClassifier(n_jobs=-1)
+    model.fit(X_train, y_train)
+
+    valid_preds = model.predict_proba(X_valid)[:, 1]
+
+    auc = metrics.roc_auc_score(y_valid, valid_preds)
+
+    print(auc)
+
+
 if __name__ == "__main__":
     for fold in range(5):
-        run(fold=fold)
+        run_v2(fold=fold)
--- chap05/src/ohe_logres.py
+++ chap05/src/ohe_logres.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn import linear_model, metrics, preprocessing
 
 
-def run(fold):
+def run_v1(fold):
     df = pd.read_csv("../input/train_folds.csv")
 
     if 0:
@@ -41,6 +41,52 @@ def run(fold):
     print(auc)
 
 
+def run_v2(fold):
+    df = pd.read_csv("../input/adult_folds.csv")
+
+    if 0:
+        print(df)
+
+    num_cols = [
+        "fnlwgt",
+        "age",
+        "capitalgain",
+        "capitalloss",
+        "hoursperweek",
+    ]
+
+    df = df.drop(columns=num_cols)
+
+    features = [f for f in df.columns if f not in ["target", "kfold"]]
+
+    for col in features:
+        df[col] = df[col].astype(str).fillna("NONE")
+
+    df_train = df[df["kfold"] != fold].reset_index(drop=True)
+    df_valid = df[df["kfold"] == fold].reset_index(drop=True)
+
+    if 0:
+        print(df_train)
+        print(df_valid)
+
+    ohe = preprocessing.OneHotEncoder()
+    ohe.fit(df[features].values)
+
+    X_train = ohe.transform(df_train[features].values)
+    y_train = df_train["target"].values
+    X_valid = ohe.transform(df_valid[features].values)
+    y_valid = df_valid["target"].values
+
+    model = linear_model.LogisticRegression()
+    model.fit(X_train, y_train)
+
+    valid_preds = model.predict_proba(X_valid)[:, 1]
+
+    auc = metrics.roc_auc_score(y_valid, valid_preds)
+
+    print(auc)
+
+
 if __name__ == "__main__":
     for fold in range(5):
-        run(fold=fold)
+        run_v2(fold=fold)

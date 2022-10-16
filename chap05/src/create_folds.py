c573f371653b49d19720d9c74f914062a061536c
room-208 <yumalegomindstorm@gmail.com>
2022-10-16 19:18:44 +0900

    foldに分割しました

--- chap05/src/create_folds.py
+++ chap05/src/create_folds.py
@@ -4,29 +4,61 @@ from sklearn import model_selection
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    df = pd.read_csv("../input/train.csv")
-
-    df["kfold"] = -1
-    kf = model_selection.StratifiedKFold(n_splits=5, shuffle=True)
-
     if 0:
-        print(df)
+        df = pd.read_csv("../input/train.csv")
 
-    X = df.drop(columns=["target", "kfold"]).values
-    y = df["kfold"].values
+        df["kfold"] = -1
+        kf = model_selection.StratifiedKFold(n_splits=5, shuffle=True)
 
-    for fold, (trn_, val_) in enumerate(kf.split(X=X, y=y)):
-        df.loc[val_, "kfold"] = fold
+        if 0:
+            print(df)
 
-    if 1:
-        print(df)
+        X = df.drop(columns=["target", "kfold"]).values
+        y = df["kfold"].values
 
-    if 1:
-        b = sns.countplot(x="target", data=df)
-        plt.show()
+        for fold, (trn_, val_) in enumerate(kf.split(X=X, y=y)):
+            df.loc[val_, "kfold"] = fold
 
-        for fold in range(5):
-            b = sns.countplot(x="target", data=df[df["kfold"] == fold])
+        if 1:
+            print(df)
+
+        if 1:
+            b = sns.countplot(x="target", data=df)
             plt.show()
 
-    df.to_csv("../input/train_folds.csv", index=False)
+            for fold in range(5):
+                b = sns.countplot(x="target", data=df[df["kfold"] == fold])
+                plt.show()
+
+        df.to_csv("../input/train_folds.csv", index=False)
+
+    if 1:
+        df = pd.read_csv("../input/adult.csv")
+
+        df["kfold"] = -1
+        kf = model_selection.StratifiedKFold(n_splits=5, shuffle=True)
+
+        df["class"] = df["class"].map(lambda x: 1 if x == ">50K" else 0)
+        df = df.rename(columns={"class": "target"})
+
+        if 0:
+            print(df)
+
+        X = df.drop(columns=["target", "kfold"]).values
+        y = df["kfold"].values
+
+        for fold, (trn_, val_) in enumerate(kf.split(X=X, y=y)):
+            df.loc[val_, "kfold"] = fold
+
+        if 0:
+            print(df)
+
+        if 1:
+            b = sns.countplot(x="target", data=df)
+            plt.show()
+
+            for fold in range(5):
+                b = sns.countplot(x="target", data=df[df["kfold"] == fold])
+                plt.show()
+
+        df.to_csv("../input/adult_folds.csv", index=False)
